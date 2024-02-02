ec39f7815e3b02a191aa6d6b07ff7a43432130ba
Mario Zupan <mario@zupzup.org>
2024-02-02 15:51:43 +0100

    add stateg

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,41 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl State<'_> {
+    async fn new(window: Window) -> Self {
+        todo!();
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!();
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!();
+    }
+
+    fn update(&mut self) {
+        todo!();
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!();
+    }
+}
+
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new().expect("can create an event lopp");
