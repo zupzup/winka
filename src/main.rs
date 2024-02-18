e5a0b2fe314b12e64d49f78e8b5c69edb47e5192
Mario Zupan <mario@zupzup.org>
2024-02-18 14:52:19 +0100

    challenge 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use winit::{
     dpi::PhysicalPosition,
-    event::{ElementState, Event, MouseButton, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
+    event::{ElementState, Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
     keyboard::{Key, NamedKey},
     platform::modifier_supplement::KeyEventExtModifierSupplement,
     window::{Window, WindowBuilder},
@@ -14,11 +14,13 @@ struct State<'window> {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
+    mouse_coords: PhysicalPosition<f64>,
 }
 
 impl<'window> State<'window> {
     async fn new(window: Window) -> State<'window> {
         let size = window.inner_size();
+        let mouse_coords = PhysicalPosition { x: 0.0, y: 0.0 };
 
         let instance = wgpu::Instance::default();
         let surface = unsafe {
@@ -59,6 +61,7 @@ impl<'window> State<'window> {
             queue,
             config,
             size,
+            mouse_coords,
         }
     }
 
@@ -75,16 +78,67 @@ impl<'window> State<'window> {
         }
     }
 
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+    fn input(&mut self, event: &WindowEvent, elwt: &EventLoopWindowTarget<()>) -> bool {
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.mouse_coords = position.to_owned();
+                true
+            }
+            WindowEvent::MouseInput { state, button, .. } => match state {
+                ElementState::Pressed => {
+                    log::info!("{button:?} mouse button pressed at {:?}", self.mouse_coords);
+                    true
+                }
+                ElementState::Released => {
+                    log::info!(
+                        "{button:?} mouse button released at {:?}",
+                        self.mouse_coords
+                    );
+                    true
+                }
+            },
+            WindowEvent::KeyboardInput { event, .. } => {
+                match event.key_without_modifiers().as_ref() {
+                    Key::Character("q") | Key::Named(NamedKey::Escape) => elwt.exit(),
+                    _ => (),
+                }
+                true
+            }
+            _ => false,
+        }
     }
 
-    fn update(&mut self) {
-        todo!();
-    }
+    fn update(&mut self) {}
 
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!();
+    fn render(&mut self, color: wgpu::Color) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(color),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        Ok(())
     }
 }
 
@@ -101,85 +155,40 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     event_loop.set_control_flow(ControlFlow::Poll);
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    let mut mouse_coords: PhysicalPosition<f64> = PhysicalPosition { x: 0.0, y: 0.0 };
-
     event_loop
-        .run(move |event, elwt| {
-            match event {
-                Event::WindowEvent { window_id, event }
-                    if window_id == state.window().id() && !state.input(&event) =>
-                {
-                    match event {
-                        WindowEvent::CloseRequested => elwt.exit(),
-                        WindowEvent::KeyboardInput { event, .. } => {
-                            match event.key_without_modifiers().as_ref() {
-                                Key::Character("q") | Key::Named(NamedKey::Escape) => elwt.exit(),
-                                _ => (),
-                            }
+        .run(move |event, elwt| match event {
+            Event::WindowEvent { window_id, event }
+                if window_id == state.window().id() && !state.input(&event, elwt) =>
+            {
+                match event {
+                    WindowEvent::CloseRequested => elwt.exit(),
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(physical_size);
+                    }
+                    WindowEvent::RedrawRequested => {
+                        let blue = state.mouse_coords.x / state.size.width as f64;
+                        let red = state.mouse_coords.y / state.size.height as f64;
+                        let color = wgpu::Color {
+                            r: red,
+                            g: red * blue,
+                            b: blue,
+                            a: 1.0,
+                        };
+                        state.update();
+                        match state.render(color) {
+                            Ok(_) => {}
+                            Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                            Err(wgpu::SurfaceError::OutOfMemory) => elwt.exit(),
+                            Err(e) => log::error!("render error: {e:?}"),
                         }
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(physical_size);
-                        }
-                        WindowEvent::RedrawRequested => {
-                            let output = state
-                                .surface
-                                .get_current_texture()
-                                .expect("can get texture");
-                            let view = output
-                                .texture
-                                .create_view(&wgpu::TextureViewDescriptor::default());
-                            let mut encoder = state.device.create_command_encoder(
-                                &wgpu::CommandEncoderDescriptor { label: None },
-                            );
-                            {
-                                let _render_pass =
-                                    encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                                        label: None,
-                                        color_attachments: &[Some(
-                                            wgpu::RenderPassColorAttachment {
-                                                view: &view,
-                                                resolve_target: None,
-                                                ops: wgpu::Operations {
-                                                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                                                        r: 0.1,
-                                                        g: 0.9,
-                                                        b: 0.3,
-                                                        a: 1.0,
-                                                    }),
-                                                    store: wgpu::StoreOp::Store,
-                                                },
-                                            },
-                                        )],
-                                        depth_stencil_attachment: None,
-                                        timestamp_writes: None,
-                                        occlusion_query_set: None,
-                                    });
-                                //     render_pass.set_pipeline(&render_pipeline);
-                                //     render_pass.draw(0..3, 0..1);
-                            }
-
-                            state.queue.submit(Some(encoder.finish()));
-                            output.present();
-                        }
-                        WindowEvent::CursorMoved { position, .. } => {
-                            mouse_coords = position;
-                        }
-                        WindowEvent::MouseInput { state, button, .. } => match state {
-                            ElementState::Pressed => {
-                                println!("{button:?} mouse button pressed at {mouse_coords:?}");
-                            }
-                            ElementState::Released => {
-                                println!("{button:?} mouse button released at {mouse_coords:?}");
-                            }
-                        },
-                        _ => (),
-                    };
-                }
-                Event::AboutToWait => {
-                    state.window.request_redraw();
-                }
-                _ => (),
+                    }
+                    _ => (),
+                };
             }
+            Event::AboutToWait => {
+                state.window.request_redraw();
+            }
+            _ => (),
         })
         .expect("event loop runs");
 }
