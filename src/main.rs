01f4769b1e37b96326a1270e2d87a79cc75721a0
Mario Zupan <mario@zupzup.org>
2024-03-12 19:53:11 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -89,36 +89,50 @@ impl Vertex {
 }
 
 const NUM_INSTANCES_PER_ROW: u32 = 10;
-const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
-    NUM_INSTANCES_PER_ROW as f32 * 0.5,
-    0.0,
-    NUM_INSTANCES_PER_ROW as f32 * 0.5,
-);
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(0.5, 0.5, 0.0);
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
+        position: [0.0, 0.1, 0.0],
         tex_coords: [0.4131759, 0.00759614],
     }, // A
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
+        position: [-0.1, -0.1, 0.0],
         tex_coords: [0.0048659444, 0.43041354],
     }, // B
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
+        position: [0.1, -0.1, 0.0],
         tex_coords: [0.28081453, 0.949397],
     }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        tex_coords: [0.85967, 0.84732914],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        tex_coords: [0.9414737, 0.2652641],
-    }, // E
+       // Vertex {
+       //     position: [-0.0868241, 0.49240386, 0.0],
+       //     tex_coords: [0.4131759, 0.00759614],
+       // }, // A
+       // Vertex {
+       //     position: [-0.49513406, 0.06958647, 0.0],
+       //     tex_coords: [0.0048659444, 0.43041354],
+       // }, // B
+       // Vertex {
+       //     position: [-0.21918549, -0.44939706, 0.0],
+       //     tex_coords: [0.28081453, 0.949397],
+       // }, // C
+       // Vertex {
+       //     position: [0.35966998, -0.3473291, 0.0],
+       //     tex_coords: [0.85967, 0.84732914],
+       // }, // D
+       // Vertex {
+       //     position: [0.44147372, 0.2347359, 0.0],
+       //     tex_coords: [0.9414737, 0.2652641],
+       // }, // E
 ];
 
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+// const VERTICES: &[Vertex] = &[
+//     Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+//     Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+//     Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+// ];
+
+const INDICES: &[u16] = &[0, 1, 2];
 
 struct State<'window> {
     surface: wgpu::Surface<'window>,
@@ -304,12 +318,12 @@ impl<'window> State<'window> {
         });
 
         let instances = (0..NUM_INSTANCES_PER_ROW)
-            .flat_map(|z| {
+            .flat_map(|y| {
                 (0..NUM_INSTANCES_PER_ROW).map(move |x| {
                     let position = cgmath::Vector3 {
-                        x: x as f32,
-                        y: 0.0,
-                        z: z as f32,
+                        x: x as f32 * 0.2,
+                        y: y as f32 * 0.2,
+                        z: 1.0,
                     } - INSTANCE_DISPLACEMENT;
 
                     Instance { position }
